154ca921565d03e7edd92d91c0623455617f2289
Robert Bieber <robby@bieberphoto.com>
2014-06-22 00:56:03 -0700

    Initial Commit
    This is a first, hacked-up iteration of the words API

---
+++ books.go
@@ -0,0 +1,107 @@
+/* Copyright (c) 2014 Robert Bieber
+ *
+ * This file is part of wordserv.
+ *
+ * wordserv is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"unicode"
+)
+
+func loadBooks(basePath string) ([][][][]string, error) {
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		return nil, err
+	}
+
+	books := [][][][]string{}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		filePath := path.Join(basePath, file.Name())
+		fin, err := os.Open(filePath)
+		if err != nil {
+			return nil, err
+		}
+		defer fin.Close()
+
+		books = append(books, readBook(fin))
+	}
+
+	_ = books
+	return books, nil
+}
+
+func readBook(fin *os.File) [][][]string {
+	chapters := [][][]string{}
+	paragraphs := [][]string{}
+	words := []string{}
+	lastLineBlank := false
+
+	scanner := bufio.NewScanner(fin)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if line == "" {
+			lastLineBlank = true
+			continue
+		} else if lastLineBlank {
+			if len(words) != 0 {
+				paragraphs = append(paragraphs, words)
+				words = []string{}
+			}
+			lastLineBlank = false
+		}
+
+		if line == "[CHAPTER]" {
+			if len(paragraphs) == 0 {
+				continue
+			}
+			chapters = append(chapters, paragraphs)
+			paragraphs = [][]string{}
+			words = []string{}
+		} else {
+			chars := []rune(line)
+			word := []rune{}
+			for _, r := range chars {
+				if unicode.IsLetter(r) || r == '\'' {
+					word = append(word, r)
+				} else {
+					if len(word) != 0 {
+						words = append(words, string(word))
+						word = []rune{}
+					}
+
+					if !unicode.IsSpace(r) {
+						words = append(words, string([]rune{r}))
+					}
+				}
+			}
+		}
+	}
+	if len(paragraphs) > 0 {
+		chapters = append(chapters, paragraphs)
+	}
+	return chapters
+}
---
+++ server.go
@@ -0,0 +1,223 @@
+/* Copyright (c) 2014 Robert Bieber
+ *
+ * This file is part of wordserv.
+ *
+ * wordserv is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+type chapterHandler struct {
+	maxChapters int
+	books       [][][][]string
+}
+
+type paragraphHandler struct {
+	maxParagraphs int
+	books         [][][][]string
+}
+
+type wordHandler struct {
+	maxWords int
+	books    [][][][]string
+}
+
+func startServer(books [][][][]string, config *Config) error {
+	http.HandleFunc("/", indexHandler)
+	http.Handle(
+		"/chapters/",
+		&chapterHandler{maxChapters: config.MaxChapters, books: books},
+	)
+	http.Handle(
+		"/paragraphs/",
+		&paragraphHandler{maxParagraphs: config.MaxParagraphs, books: books},
+	)
+	http.Handle(
+		"/words/",
+		&wordHandler{maxWords: config.MaxWords, books: books},
+	)
+	return http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write(
+		[]byte(
+			`<!DOCTYPE html>
+<html>
+    <head><title>Words API</title></head>
+    <body>
+        <h1>Words API</h1>
+        <p>
+            You can use the words API to fetch random snippets of public domain
+            English literature.  Just send a GET request to
+            <tt>/denomination/count/</tt>, where <tt>count</tt> is the number
+            of things that you want and <tt>denomination</tt> is one of
+            the following:
+        </p>
+        <ul>
+            <li><tt>chapters</tt></li>
+            <li><tt>paragraphs</tt></li>
+            <li><tt>words</tt></li>
+        </ul>
+        <p>
+            The response will be JSON text representing one or more levels of
+            nested lists.  For <tt>words</tt> requests, the response will be a
+            one-layer deep list of words.  For <tt>paragraphs</tt> requests, it
+            will be a list of lists of words, where each sub-list is a single
+            paragraph.  For <tt>chapters</tt> requests, it will be a list of
+            lists of lists, where the outer-most sublists are chapters and the
+            inner-most sublists are paragraphs.
+        </p>
+        <p>
+            <strong>
+                Note that you should not depend on this server.  It may break
+                or disappear at any time and fetches from a limited set of
+                works.  If you need to use this API for your own purposes, you
+                should host a copy on your own servers.
+            </strong>
+        </p>
+        Source Code:
+        <a href="http://www.github.com/bieber/wordserv/">
+            http://www.github.com/bieber/wordserv/
+        </a>
+    </body>
+</html>
+`,
+		),
+	)
+}
+
+func (c chapterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	pattern, _ := regexp.Compile("^/chapters/(\\d+)/?$")
+	matches := pattern.FindStringSubmatch(r.URL.Path)
+	if matches == nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	chapterCount, _ := strconv.Atoi(matches[1])
+	book := c.books[rand.Intn(len(c.books))]
+	chapterCount = min(chapterCount, c.maxChapters, len(book))
+
+	startChapter := rand.Intn(min(len(book)-chapterCount, 1))
+	chapters := book[startChapter : startChapter+chapterCount]
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(chapters)
+}
+
+func (p paragraphHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	pattern, _ := regexp.Compile("^/paragraphs/(\\d+)/?$")
+	matches := pattern.FindStringSubmatch(r.URL.Path)
+	if matches == nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	paragraphCount, _ := strconv.Atoi(matches[1])
+	paragraphCount = min(paragraphCount, p.maxParagraphs)
+
+	book := p.books[rand.Intn(len(p.books))]
+	paragraphsCounted := 0
+	maxChapter := 0
+	for maxChapter = len(book) - 1; maxChapter >= 0; maxChapter-- {
+		paragraphsCounted += len(book[maxChapter])
+		if paragraphsCounted >= paragraphCount {
+			break
+		}
+	}
+	maxChapter = max(maxChapter, 1)
+
+	paragraphs := [][]string{}
+	chapter := rand.Intn(maxChapter)
+	paragraph := rand.Intn(len(book[chapter]))
+	for len(paragraphs) < paragraphCount {
+		if chapter >= len(book) {
+			break
+		}
+		if paragraph >= len(book[chapter]) {
+			chapter++
+			paragraph = 0
+		}
+
+		paragraphsToAdd := min(
+			paragraphCount-len(paragraphs),
+			len(book[chapter])-paragraph,
+		)
+		paragraphs = append(
+			paragraphs,
+			book[chapter][paragraph:paragraph+paragraphsToAdd]...,
+		)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(paragraphs)
+}
+
+func (w wordHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	pattern, _ := regexp.Compile("^/words/(\\d+)/?$")
+	matches := pattern.FindStringSubmatch(r.URL.Path)
+	if matches == nil {
+		http.NotFound(rw, r)
+		return
+	}
+
+	wordCount, _ := strconv.Atoi(matches[1])
+	wordCount = min(wordCount, w.maxWords)
+
+	book := w.books[rand.Intn(len(w.books))]
+	wordsCounted := 0
+	maxChapter := 0
+	for maxChapter = len(book) - 1; maxChapter >= 0; maxChapter-- {
+		for _, c := range book[maxChapter] {
+			wordsCounted += len(c)
+		}
+		if wordsCounted >= wordCount {
+			break
+		}
+	}
+	maxChapter = max(maxChapter, 1)
+
+	words := []string{}
+	chapter := rand.Intn(maxChapter)
+	paragraph := rand.Intn(len(book[chapter]))
+	for len(words) < wordCount {
+		if chapter >= len(book) {
+			break
+		}
+		if paragraph >= len(book[chapter]) {
+			paragraph = 0
+			chapter++
+		}
+
+		wordsToAdd := min(
+			wordCount-len(words),
+			len(book[chapter][paragraph]),
+		)
+		words = append(
+			words,
+			book[chapter][paragraph][0:wordsToAdd]...,
+		)
+		chapter++
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(words)
+}
